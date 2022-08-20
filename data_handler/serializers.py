b75333a6df083f3507b74a009204da0cc4b3ccb5
Vsevolod Bezaev <vvbezaev@gmail.com>
2022-08-20 20:51:19 +0300

    Change gRPC library to modern one

--- data_handler/serializers.py
+++ data_handler/serializers.py
@@ -1,21 +1,18 @@
-from django_grpc_framework import proto_serializers
+from rest_framework.serializers import ModelSerializer
+
 from data_handler.models import Category, MeasureUnit, Product
-from proto import category_pb2, measure_unit_pb2, product_pb2
 
 
-class CategoryProtoSerializer(proto_serializers.ModelProtoSerializer):
+class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
-        proto_class = category_pb2.Category
 
 
-class MeasureUnitProtoSerializer(proto_serializers.ModelProtoSerializer):
+class MeasureUnitSerializer(ModelSerializer):
     class Meta:
         model = MeasureUnit
-        proto_class = measure_unit_pb2.MeasureUnit
 
 
-class ProductProtoSerializer(proto_serializers.ModelProtoSerializer):
+class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
-        proto_class = product_pb2.Product
--- data_handler/views.py
+++ data_handler/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from grpc_django.views import RetrieveGRPCView, ServerStreamGRPCView
+from data_handler.models import Category, MeasureUnit, Product
+from data_handler.serializers import CategorySerializer, MeasureUnitSerializer, ProductSerializer
+from grpc_codegen.data_handler_pb2 import Category as CategoryProto, MeasureUnit as MeasureUnitProto, Product as ProductProto
 
-# Create your views here.
+
+class RetrieveCategory(RetrieveGRPCView):
+    queryset = Category.objects.all()
+    response_proto = CategoryProto
+    serializer_class = CategorySerializer
+
+
+class ListCategories(ServerStreamGRPCView):
+    queryset = Category.objects.all()
+    response_proto = CategoryProto
+    serializer_class = CategorySerializer
+
+
+class RetrieveMeasureUnit(RetrieveGRPCView):
+    queryset = MeasureUnit.objects.all()
+    response_proto = MeasureUnitProto
+    serializer_class = MeasureUnitSerializer
+
+
+class ListMeasureUnits(ServerStreamGRPCView):
+    queryset = MeasureUnit.objects.all()
+    response_proto = MeasureUnitProto
+    serializer_class = MeasureUnitSerializer
+
+
+class RetrieveProduct(RetrieveGRPCView):
+    queryset = Product.objects.all()
+    response_proto = ProductProto
+    serializer_class = ProductSerializer
+
+
+class ListProducts(ServerStreamGRPCView):
+    queryset = Product.objects.all()
+    response_proto = ProductProto
+    serializer_class = ProductSerializer
