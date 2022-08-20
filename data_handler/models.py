5f91405ee47de8e7e703881a1b921ed079c40791
Vsevolod Bezaev <vvbezaev@gmail.com>
2022-08-20 06:00:50 +0300

    Create separate django app with models.py and changed docker-compose volumes config

--- data_handler/models.py
+++ data_handler/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as l_
+from django.utils.translation import gettext_lazy as l_
 
 from mptt.fields import TreeForeignKey
 from mptt.models import MPTTModel
 
 
-
 class Category(MPTTModel):
-    parent = models.ForeignKey('self', blank=True, null=True, verbose_name=l_('Родительская категория'), related_name='child')
+    parent = models.ForeignKey('self', blank=True, null=True, verbose_name=l_('Родительская категория'),
+                               related_name='child')
     name = models.CharField(max_length=550)
 
     class Meta:
@@ -15,19 +15,16 @@ class Category(MPTTModel):
         verbose_name_plural = l_('Категории товаров')
 
 
-
 class MeasureUnit(models.Model):
     name = models.CharField(max_length=255, verbose_name=l_('Название'))
-    full_name = models.CharField(max_length=255, blank=True, default='',verbose_name=l_('Полное название'))
+    full_name = models.CharField(max_length=255, blank=True, default='', verbose_name=l_('Полное название'))
 
     class Meta:
         verbose_name = l_('Единица измерения')
         verbose_name_plural = l_('Единицы измерения')
 
 
-
 class Product(models.Model):
-
     PRODUCT = 0
     SERVICE = 1
 
@@ -43,7 +40,8 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True, verbose_name=l_('Описание'))
     product_type = models.IntegerField(choices=TYPES, default=PRODUCT, verbose_name=l_('Тип номенклатуры'))
     rate_nds = models.FloatField(default=20, verbose_name=l_('Ставка НДС'))
-    measure_unit = models.ForeignKey(MeasureUnit, null=True, blank=True, verbose_name=l_('Единица измерения'), on_delete=models.SET_NULL)
+    measure_unit = models.ForeignKey(MeasureUnit, null=True, blank=True, verbose_name=l_('Единица измерения'),
+                                     on_delete=models.SET_NULL)
     hidden = models.BooleanField(default=False, verbose_name=l_('Скрывать товар в Agora'), db_index=True)
 
     class Meta:
